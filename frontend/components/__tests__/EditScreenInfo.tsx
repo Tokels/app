94c635fd6762eacb54420d588c4b55a5a4500a54
zerodeleo <julia.stjerna@hotmail.se>
2023-11-28 12:08:14 +0100

    setup jest TS

--- frontend/components/__tests__/EditScreenInfo.tsx
+++ frontend/components/__tests__/EditScreenInfo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import renderer from 'react-test-renderer';
 import { Text } from 'react-native';
 
-it(`renders correctly`, () => {
+it('renders correctly', () => {
   const tree = renderer.create(<Text>Snapshot test!</Text>).toJSON();
 
   expect(tree).toMatchSnapshot();
